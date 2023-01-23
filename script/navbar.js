20891a8330ca46ef771cd96649a67b7e89768259
itZzenXX <itzzen@pm.me>
2023-01-23 11:07:55 -0500

    Remove all JS
    Signed-off-by: itZzenXX <itzzen@pm.me>

--- script/navbar.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 0) {
-            $('.navigation').addClass('navigationChangeColor')
-        } else {
-            $('.navigation').removeClass('navigationChangeColor')
-        }
-    });
-});
